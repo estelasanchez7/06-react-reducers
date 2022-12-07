8ecfba92c222f4d0017cefad07bb9f5c7e6da7fd
estelasanchez7 <stelasv7@gmail.com>
2022-12-07 21:38:02 +0100

    Hook_useReducer

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,15 +1,29 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 
 const initialState = { contador: 0 };
 
+const TYPES = {
+  INCREMENT: "INCREMENT",
+  INCREMENT_5: "INCREMENT_5",
+  DECREMENT: "DECREMENT",
+  DECREMENT_5: "DECREMENT_5",
+  RESET: "RESET",
+};
+
 function reducer(state, action) {
   switch (action.type) {
-    case "INCREMENT":
+    case TYPES.INCREMENT:
       return { contador: state.contador + 1 };
-    case "DECREMENT":
+    case TYPES.INCREMENT_5:
+      return { contador: state.contador + action.payload };
+    case TYPES.DECREMENT:
       return { contador: state.contador - 1 };
+    case TYPES.DECREMENT_5:
+      return { contador: state.contador - action.payload };
+      case TYPES.RESET:
+        return initialState
     default:
-        return state;
+      return state;
   }
 }
 
@@ -18,17 +32,26 @@ const Contador = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   //const sumar = () => setContador(contador + 1);
-  const sumar = () => dispatch({type:"INCREMENT"})
+  const sumar = () => dispatch({ type: TYPES.INCREMENT });
+
+  const sumar5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
 
   //const restar = () => setContador(contador - 1)
-  const restar = () => dispatch({type:"DECREMENT"})
+  const restar = () => dispatch({ type: TYPES.DECREMENT });
+
+  const restar5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+
+  const reset =()=> dispatch({type: TYPES.RESET})
 
   return (
     <div style={{ textAlign: "center" }}>
       <h2>Contador Reducer</h2>
       <nav>
+        <button onClick={sumar5}>+5</button>
         <button onClick={sumar}>+</button>
+        <button onClick={reset}>0</button>
         <button onClick={restar}>-</button>
+        <button onClick={restar5}>-5</button>
       </nav>
       <h3>{state.contador}</h3>
     </div>
