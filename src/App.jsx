93f37bf384c85b133f11659777a50f820a1de245
estelasanchez7 <stelasv7@gmail.com>
2022-12-07 20:59:40 +0100

    Reducers

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>useReducer</h1>
+      <hr />
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
