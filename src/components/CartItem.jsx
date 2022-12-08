9731c067d1fafe5d595441b63b4f67b2d1431661
estelasanchez7 <stelasv7@gmail.com>
2022-12-08 14:53:11 +0100

    Carrito_Compras_Reducers_4

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,10 @@
 const CartItem = ({data, delFromCart}) => {
-    let {id, name, price} = data
+    let {id, name, price, quantity} = data
 
   return (
     <div style={{borderBottom:"thin solid gray"}}>
         <h4>{name}</h4>
-        <h5>€{price}.00</h5>
+        <h5>€{price}.00 x {quantity} = €{price * quantity}.00</h5>
         <button>Eliminar Uno</button>
         <button>Eliminar Todos</button>
     </div>
--- src/reducers/shoppingReducer.jsx
+++ src/reducers/shoppingReducer.jsx
@@ -20,11 +20,20 @@ export function shoppingReducer(state, action) {
       );
       //console.log(newItem);
 
-      return {
-        ...state,
-        cart:[...state.cart,newItem],
-      }
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
+
+      return itemInCart
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+          }
+        : { ...state, cart: [...state.cart, {...newItem, quantity: 1 }] };
     }
+
     case TYPES.REMOVE_ONE_FROM_CART: {
     }
     case TYPES.REMOVE_ALL_FROM_CART: {
