ce7923c1c9b4395e5252f67ba93d4e4ea1d810f3
estelasanchez7 <stelasv7@gmail.com>
2022-12-08 10:30:18 +0100

    Carrito_Compras_Reducers_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Contador from "./components/Contador";
 import ContadorMejorado from "./components/ContadorMejorado";
+import ShopingCart from "./components/ShopingCart";
 
 function App() {
   return (
     <div className="App">
       <h1>useReducer</h1>
+      <hr />
+      <ShopingCart/>
       <hr />
       <ContadorMejorado/>
       <hr />
---
+++ src/components/ShopingCart.jsx
@@ -0,0 +1,13 @@
+const ShopingCart = () => {
+  return (
+    <div>
+        <h2>Carrito de Compras</h2>
+        <h3>Productos</h3>
+        <arcticle className="box"></arcticle>
+        <h3>Carrito</h3>
+        <article className="box"></article>
+    </div>
+  )
+}
+
+export default ShopingCart
